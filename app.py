6d1015cb2e8cf82e5749208ffdbd744f9a00890d
samuel20431 <balajikrishnan2662001@gmail.com>
2026-09-06 07:12:23 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 import streamlit as st
-st.title("welcome to  gnc college it is very goated college with goated teachers and students")
+st.title("login page")
+class login:
+    status = "active"
+    def __init__(self, username,password,mark):
+        self.username = username
+        self.password = password
+        self.mark = mark 
+
+    def get_total(self):
+        return sum(self.mark)
+
+result = login("samuel","samuel123",{48,96,80,86,97})
+print(result.username)
+print(result.password)
+print("total mark",result.get_total())
