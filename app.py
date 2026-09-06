b1a6e4db8b869ca65061e31efdeb893cd26bb215
samuel20431 <balajikrishnan2662001@gmail.com>
2026-09-06 07:59:08 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -6,7 +6,13 @@ from db_helper import create_table
 
 def main():
     create_table()
-st.title("Student Login System")
+    st.title("Student Login System")
+    choice = st.sidebar.selectbox("Select an option", ["Login", "Register"])
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    st.button("login")
+    
+
 
 if __name__ == "__main__":
     main()
